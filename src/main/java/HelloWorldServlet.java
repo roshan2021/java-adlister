12d80e2b31a6bde6765e0573a28770b013583d82
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-06-08 22:34:07 -0500

    servlets-exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,27 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
 
+        String name = request.getParameter("name");
+        System.out.println("name param = " + name);
+
+        //If null print out default Hello World
+        if(name == null) {
+            out.println("<strong>Hello World!</strong>");
+        //If not null, print out something else
+        } else {
+            out.println("<strong>Hello, " + name + "</strong>");
+        }
     }
 }
