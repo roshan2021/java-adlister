afc8159d3be633e476eb29b3c8cc530414aa1506
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-06-10 23:02:03 -0500

    sessions cookies exercise

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -4,12 +4,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
-@WebServlet(name = "AdsIndexServlet", urlPatterns = "/ads")
-public class AdsIndexServlet extends HttpServlet {
+@WebServlet(name = "LogoutServlet", urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("ads", DaoFactory.getAdsDao().all());
-        request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
+        //Gets rids of the entire session
+        request.getSession().invalidate();
+        //Redirects to Login Page to login again if they want to
+        response.sendRedirect("/login");
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,12 +2,14 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <jsp:include page="partials/head.jsp">
+<%--    this is sever side--%>
+    <jsp:include page="WEB-INF/partials/head.jsp">
         <jsp:param name="title" value="Welcome to my site!" />
     </jsp:include>
 </head>
 <body>
-    <jsp:include page="partials/navbar.jsp" />
+<%--this is server side--%>
+    <jsp:include page="WEB-INF/partials/navbar.jsp" />
     <div class="container">
         <h1>Welcome to the Adlister!</h1>
     </div>
